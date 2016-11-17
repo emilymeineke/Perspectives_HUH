fa572625cb9804b8761ebf13c08fe4138e60032d
emilymeineke <emily.meineke@gmail.com>
2016-11-17 09:29:58 -0500

    more work on figures

--- rosid_asterid_herbivory/rosid_asterid_rscript.R
+++ rosid_asterid_herbivory/rosid_asterid_rscript.R
@@ -5,6 +5,12 @@ library(plyr)
 RA_dframe <- read.csv("rosid_asterid_data.csv")
 names(RA_dframe)
 attach(RA_dframe)
+RA_dframe$date <- as.Date(RA_dframe$date)
+
+#split genus in species in case I need them separated
+RA_dframe$plant_genus_species_split <- RA_dframe$plant_genus_species
+bind <- data.frame(do.call('rbind', strsplit(as.character(RA_dframe$plant_genus_species_split),'_',fixed=TRUE)))
+RA_dframe <- cbind(RA_dframe, bind)
 
 RA_dframe$leafmines_tot <- RA_dframe$serpentine.mine+RA_dframe$blotch.mine
 RA_dframe$chewing_tot <- RA_dframe$ch_leafremoved+RA_dframe$skel
@@ -79,29 +85,60 @@ detach(RA_dframe)
 
 #Making paper figures
 
-#Figure 1: Zero inflation
+#Figure 1: Variation among species
+#cells with chewing damage by taxon & species
+#order dataframe
+x <- subset(RA_dframe, ch_leafremoved != "NA")
+
+italic.text <- element_text(face = "italic", color = "black")
+bplot2 <- ggplot(x, aes(x=reorder(plant_genus_species,ch_leafremoved, mean), y=ch_leafremoved)) 
+bplot2 <- bplot2 + geom_boxplot() + theme_bw() + theme(axis.text.x = element_text(angle=60, hjust=1)) + ylab("Grid cells with herbivory") +
+  xlab("") + theme(legend.position= "none") + scale_x_discrete("", labels = c("baptisia_tinctoria"="Baptisia tinctoria",    "clethra_alnifolia"="Clethra alnifolia",     "cornus_racemosa"="Cornus racemosa",       "epigaea_repens"="Epigaea repens",
+                                                                              "galium_triflorum"="Galium triflorum",      "gaylussacia_baccata"="Gaylussacia baccata",  "kalmia_angustifolia"="Kalmia angustifolia",   "lechea_intermedia"="Lechea intermedia",
+                                                                              "lespedeza_capitata"="Lespedeza capitata",    "lespedeza_hirta"="Lespedeza hirta",       "ludwigia_palustris"="Ludwigia palustris",    "lycopus_americanus"="Lycopus americanus",   
+                                                                              "lysimachia_terrestris"="Lysimachia terrestris", "mentha_arvensis"="Mentha arvensis",       "polygala_sanguinea"="Polygala sanguinea",    "triadenum_virginicum"="Triadenum virginicum",
+                                                                              "vaccinium_macrocarpon"="Vaccinium macrocarpon", "viola_blanda"="Viola blanda", "viola_cucullata"="Viola cucullata",       "vitis_labrusca"="Vitis labrusca")) +
+  theme(axis.text.x = italic.text)
+bplot2
+
+
+
+
+#Figure 2: Zero inflation
 #What are the most prevalent 5 types of herbivory? 
 RA_dframe_resh$binary <- ifelse(RA_dframe_resh$value>0, 1, 0)
 topfive <- ddply(RA_dframe_resh, ~variable, summarize, mean_specdamaged = mean(binary, na.rm=T))
 topfive <- topfive[order(-topfive$mean_specdamaged),]
 #topfive = chewing_tot, stippling, sooty.mould, leafmines_tot, leaf.galls
 
-topfive_RA_dframe_resh <- subset(RA_dframe_resh, variable=="chewing_tot"|variable=="stippling"|variable=="sooty.mould"|variable=="leafmines_tot"|
-                                   variable=="leaf.galls")
+topfive_RA_dframe_resh <- subset(RA_dframe_resh, variable=="ch_leafremoved"|variable=="skel"|variable=="stippling"|variable=="sooty.mould"|variable=="leafmines_tot"|variable=="leaf.galls")
 
-Fig1 <- ggplot(topfive_RA_dframe_resh, aes(value, colour=variable)) 
-Fig1 <- Fig1 +  geom_freqpoly(binwidth = 1) +xlim(0,5) +theme_bw() +xlab("cells with herbivore damage") + ylab("specimens")
-Fig1
+#Freqency poly
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+Fig2 <- ggplot(topfive_RA_dframe_resh, aes(value, colour=variable)) 
+Fig2 <- Fig2 +  geom_freqpoly(binwidth = 1, size=1.3) +xlim(0,5) +theme_bw() +xlab("Grid cells with herbivory") + ylab("Specimens") + scale_colour_manual(values=cbPalette, 
+                                                                                                                                                          name="Damage Type",
+                                                                                                                                                          breaks=c("ch_leafremoved", "skel", "stippling", "leaf.galls", "sooty.mould", "leafmines_tot"),
+                                                                                                                                                          labels=c("Chewing (leaf area removed)", "Skeletonization", "Stippling", "Leaf Galls", "Sooty Mould", "Leaf mines"))
+Fig2
+
+#Histogram
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+hist_cut <- ggplot(topfive_RA_dframe_resh, aes(x=value, fill=variable)) +xlab("Grid cells with herbivory") + ylab("Specimens")
+hist_cut <- hist_cut + geom_bar(position="dodge") + theme_bw() + scale_fill_manual(values=cbPalette, 
+                                                                        name="Damage Type",
+                                                                        breaks=c("ch_leafremoved", "skel", "stippling", "leaf.galls", "sooty.mould", "leafmines_tot"),
+                                                                        labels=c("Chewing (leaf area removed)", "Skeletonization", "Stippling", "Leaf Galls", "Sooty Mould", "Leaf mines"))
+hist_cut
 
 
 
-#Figure 2: Progression of herbivory annually
+#Figure 3: Progression of herbivory annually
 
-
-#Continuous
-Fig2_cont <- ggplot(topfive_RA_dframe_resh, aes(x=doy, y=value, colour=plant_genus_species)) 
-Fig2_cont <- Fig2_cont +  geom_point() +ylim(0,5) +theme_bw() +xlab("day of year") + ylab("cells with herbivore damage") 
-Fig2_cont
+#Continuous, garbage
+Fig3_cont <- ggplot(topfive_RA_dframe_resh, aes(x=doy, y=value, colour=plant_genus_species)) 
+Fig3_cont <- Fig3_cont +  geom_point() +ylim(0,5) +theme_bw() +xlab("day of year") + ylab("cells with herbivore damage") 
+Fig3_cont
 
 #Get incidence of chewing herbivory for all species
 #Plot how chewing herbivory changes over season for all species
@@ -189,11 +226,14 @@ plo
 plo <- ggplot(VILA, aes(x=doy, y=ch_leafremoved, colour=plant_genus_species)) + geom_point() + geom_abline()
 plo
 
+
+
 #Cat
 #Format data
 RA_dframe$totalherbiv <- RA_dframe$chewing_tot+RA_dframe$stippling+RA_dframe$leafmines_tot+RA_dframe$sooty.mould+RA_dframe$leaf.galls
 RA_dframe$binary_noherbiv <- ifelse(RA_dframe$totalherbiv>0, 1, 0)
-RA_dframe$binary_chew<- ifelse(RA_dframe$chewing_tot>0, 1, 0)
+RA_dframe$binary_chew<- ifelse(RA_dframe$ch_leafremoved>0, 1, 0)
+RA_dframe$binary_skel <- ifelse(RA_dframe$skel>0, 1, 0)
 RA_dframe$binary_stipp <- ifelse(RA_dframe$stippling>0, 1, 0)
 RA_dframe$binary_lmines <- ifelse(RA_dframe$leafmines_tot>0, 1, 0)
 RA_dframe$binary_SM <- ifelse(RA_dframe$sooty.mould>0, 1, 0)
@@ -201,10 +241,10 @@ RA_dframe$binary_lgalls <- ifelse(RA_dframe$leaf.galls>0, 1, 0)
 
 RA_dframe_resh2 <- melt(RA_dframe, id.vars=c("plant_genus_species", "taxon", "MF", "abscission_num",
                                             "herbarium", "date", "month", "month_cat", "day", "year", "doy", 
-                                            "state", "phenology", "county"))
+                                            "state", "phenology", "county", "X1", "X2"))
 
 RA_dframe_resh2_fig2 <- subset(RA_dframe_resh2, variable== "binary_noherbiv"|variable=="binary_chew"|variable=="binary_stipp"|variable=="binary_lmines"|
-                                 variable=="binary_SM"|variable=="binary_lgalls")
+                                 variable=="binary_SM"|variable=="binary_lgalls"|variable=="binary_skel")
 
 mar <- subset(RA_dframe_resh2_fig2, month_cat=="March")
 apr <- subset(RA_dframe_resh2_fig2, month_cat=="April")
@@ -215,31 +255,28 @@ aug <- subset(RA_dframe_resh2_fig2, month_cat=="August")
 sept <- subset(RA_dframe_resh2_fig2, month_cat=="September")
 oct <- subset(RA_dframe_resh2_fig2, month_cat=="October")
 
-#Work on this
-prop <- function(x)  { for (i in 1:length(x)) {
-  x$prop = x$value/nrow(x)
-}
-}
+#Plot
+cat <- ggplot(RA_dframe_resh2_fig2, aes(value)) 
+cat <- cat + geom_bar(aes(fill = variable), position = "fill")
+cat
 
-mar$prop <- mar$value/nrow(mar)
-apr$prop <- apr$value/nrow(apr)
-may$prop <- may$value/nrow(may)
-jun$prop <- jun$value/nrow(jun)
-jul$prop <- jul$value/nrow(jul)
-aug$prop <- aug$value/nrow(aug)
-sept$prop <- sept$value/nrow(sept)
-oct$prop <- oct$value/nrow(oct)
-
-props_summary <- rbind(mar, apr, may, jun, jul, aug, sept, oct)
-
-#props_summary <- ddply(RA_dframe_resh2_fig2, c("month_cat", "variable"), summarize, sum_wtype = sum(value, na.rm=T))
-
-props_summary <- subset(props_summary, month<10 & month>3)
-Fig2_cat <- ggplot(props_summary, aes(x=month_cat, y=prop, fill=variable)) + geom_bar(stat="identity") +theme_bw() + ylab("cells with herbivore damage") 
-Fig2_cat
+RA_dframe_resh2_fig2$value <- as.numeric(RA_dframe_resh2_fig2$value)
+x <- nrow(RA_dframe_resh2_fig2)
+RA_dframe_fig2_noNA <- subset(RA_dframe_resh2_fig2, value != "NA")
+RA_dframe_fig2_noNA$prop <- RA_dframe_fig2_noNA$value/4908
+RA_dframe_fig2_noNA <- subset(RA_dframe_fig2_noNA, month_cat != "March" & month_cat != "December")
 
 
+cat <- ggplot(RA_dframe_fig2_noNA, aes(x=reorder(month_cat,doy), y=prop)) + xlab("") + ylab("Proportion of specimens")
+cat <- cat + geom_bar(aes(fill = variable), position = "fill", stat="identity")
+cat
+
+
+#Maybe make the plot above for species with the highest incidence of herbivory? 
+
+
+x=reorder(plant_genus_species,ch_leafremoved, mean)
 
 #Test for effect of year, doy
-model <- lme(ch_leafremoved~doy+year+state, random = ~1|plant_genus_species, na.action=na.omit, data=RA_dframe)
+model <- lme(ch_leafremoved~doy, random = ~1|plant_genus_species, na.action=na.omit, data=RA_dframe)
 summary(model)
